beafa608eeec37a990f1741b44cbab458967c577
Danil <zogame002@gmail.com>
2023-08-01 17:54:56 +0500

    deploy configs

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { MenuComponent } from './components';
-
-function App() {
-  return (
-      <MenuComponent />
-  )
-}
-
-export default App
--- src/mock/index.ts
+++
@@ -1,1 +0,0 @@
-export { emails } from "./data";
